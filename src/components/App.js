7d842ae3dd07173d5998bb7c9d648089a7a2f981
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2021-01-14 21:21:15 -0500

    compress

--- src/components/App.js
+++ src/components/App.js
@@ -11,16 +11,15 @@ import Landscape from "./Landscape.js";
 import ScrollToTop from "./ScrollToTop";
 import About from "./About";
 import Contact from "./Contact";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [firstLoad, setFirstLoad] = useState(true);
   return (
     <Router>
       <ScrollToTop />
       <Switch>
         <Route exact path="/">
-          <Portraits firstLoad={firstLoad} setFirstLoad={setFirstLoad} />
+          <Portraits />
         </Route>
         <Route exact path="/portrait">
           <Portraits />
